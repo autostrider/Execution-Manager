2d26583c8209d8af0339ec41746c1f9b0bb860df
Ihor Berizka <ihor.berizka@gmail.com>
2019-08-22 10:08:38 +0300

    Added transition to kTerminating state when received SIGTERM

--- applications/AdaptiveApplications/TestApp/src/aa_main.cpp
+++ applications/AdaptiveApplications/TestApp/src/aa_main.cpp
@@ -5,28 +5,25 @@
 
 static void siginthandler(int signo);
 
-//using fsm_handle = App;
-
 int main()
 {
     if (::signal(SIGINT, siginthandler) == SIG_ERR)
     {
         std::cout << "Error while registering signal\n";
-        return 1;
     }
     std::cout << "app1\tproc1\n";
 
-    App/*fsm_handle*/::start();
-   // fsm_handle::dispatch(kInitializing());
-    App/*fsm_handle*/::dispatch(kRunning());
-    App/*fsm_handle*/::dispatch(kTerminating());
+    App::start();
+    App::dispatch(kRunning());
+    App::dispatch(kTerminating());
     return 0;
 }
 
 static void siginthandler(int signo)
 {
-    std::cout << "received signal:" << sys_siglist[signo];
-    ::exit(1);
+    std::cout << "received signal:" << sys_siglist[signo] << "\n";
+    App::dispatch(kTerminating());
+    ::exit(EXIT_SUCCESS);
 }
 
 

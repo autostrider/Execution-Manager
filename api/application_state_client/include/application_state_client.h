b484eed29ced5fb288a1e5192f8e6db72488a0f8
Rostyslav Bekesh <54239849+rBekesh@users.noreply.github.com>
2019-09-12 18:14:23 +0300

    temporary changes

--- api/application_state_client/include/application_state_client.h
+++ api/application_state_client/include/application_state_client.h
@@ -1,7 +1,5 @@
 #ifndef APPLICATION_STATE_CLIENT_H
 #define APPLICATION_STATE_CLIENT_H
-#include <sys/types.h>
-#include <unistd.h>
 #include <application_state_management.capnp.h>
 #include <capnp/ez-rpc.h>
 
@@ -19,6 +17,20 @@ private:
   capnp::EzRpcClient m_client;
 
   pid_t m_pid;
+};
+
+class IApplicationStateClientWrapper
+{
+public:
+    virtual void ReportApplicationState(ApplicationStateManagement::ApplicationState state) = 0;
+};
+
+class ApplicationStateClientWrapper : public IApplicationStateClientWrapper
+{
+public:
+    void ReportApplicationState(ApplicationStateManagement::ApplicationState state) override;
+private:
+    ApplicationStateClient m_client;
 };
 
 } // namespace api
--- applications/MachineStateManager/src/msm_main.cpp
+++ applications/MachineStateManager/src/msm_main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 #include "machine_state_manager.hpp"
+#include <msm_state_machine.hpp>
+
+#include <signal.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <atomic>
+
+static void signalHandler(int signo);
+static std::atomic<bool> isTerminating{false};
 
 int main(int argc, char **argv)
 {
-  MachineStateManager::MachineStateManager msm;
+    if (::signal(SIGTERM, signalHandler) == SIG_ERR)
+    {
+        std::cout << "[ MachineStateMAnager ] :\tError while registering signal" << std::endl;
+    }
 
-  return msm.start();
+    MachineStateManager msm(std::make_unique<StateFactory>(),
+                            std::make_unique<api::ApplicationStateClientWrapper>());
+
+    while (!isTerminating)
+    {
+        msm.run();
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+    msm.terminate();
+    return 0;
+}
+
+static void signalHandler(int signo)
+{
+    std::cout << "[ MachineStateMAnager ] :\tReceived signal:" << sys_siglist[signo] << std::endl;
+    isTerminating = true;
 }

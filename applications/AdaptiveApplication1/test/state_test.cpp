b9d52e7e30c98808ac4372d4860954f3fff904bc
ihor <ihor.berizka@gmail.com>
2019-09-13 20:56:43 +0300

    Few tests for State subclasses

--- applications/AdaptiveApplication1/test/state_test.cpp
+++ applications/AdaptiveApplication1/test/state_test.cpp
@@ -32,15 +32,30 @@ public:
     MOCK_METHOD1(ReportApplicationState, void(ApplicationStateManagement::ApplicationState state));
 };
 
-class IAdaptiveAppMock : public api::IAdaptiveApp
+class IAdaptiveAppMock : public AdaptiveApp
 {
 public:
-    MOCK_METHOD(void, run,());
-    MOCK_METHOD(void, terminate,());
+    IAdaptiveAppMock(std::unique_ptr<api::IStateFactory> factory,
+    std::unique_ptr<api::IApplicationStateClientWrapper> client);
+
+    MOCK_METHOD(void, init, ());
+    MOCK_METHOD(void, run, ());
+    MOCK_METHOD(void, terminate, ());
 
     MOCK_METHOD(void, reportApplicationState,
                 (api::ApplicationStateClient::ApplicationState state));
+    MOCK_METHOD(void, transitToNextState, (IAdaptiveApp::FactoryFunc nextState));
+
+    MOCK_METHOD(double, mean, ());
+    MOCK_METHOD(void, readSensorData, ());
 };
+
+IAdaptiveAppMock::IAdaptiveAppMock(std::unique_ptr<api::IStateFactory> factory,
+                                   std::unique_ptr<api::IApplicationStateClientWrapper> client) :
+    AdaptiveApp (std::move(factory), std::move(client))
+{
+
+}
 
 class StateTest : public ::testing::Test
 {
@@ -49,15 +64,48 @@ protected:
     {
         stateClientMock = std::make_unique<StateClientMock>();
         factoryMock = std::make_unique<IStateFactoryMock>();
+        appMock = new IAdaptiveAppMock{std::move(factoryMock), std::move(stateClientMock)};
     }
+
+    void TearDown() override
+    {
+        delete appMock;
+    }
+
    std::unique_ptr<StateClientMock> stateClientMock{nullptr};
    std::unique_ptr<IStateFactoryMock> factoryMock{nullptr};
+   IAdaptiveAppMock* appMock{nullptr};
 };
 
-class FactoryTest : public ::testing::Test
+TEST_F(StateTest, Should_InitCallEnter)
 {
+    EXPECT_CALL(*appMock, reportApplicationState(_)).WillOnce(Return());
 
-protected:
-    IStateFactoryMock factoryMock;
-};
+    std::unique_ptr<Init> state = std::make_unique<Init>(*appMock);
+    state->enter();
+}
 
+TEST_F(StateTest, Should_RunCallEnter)
+{
+    //EXPECT_CALL(*appMock, mean()).WillOnce(Return(0));
+    //EXPECT_CALL(*appMock, readSensorData()).WillOnce(Return());
+
+    std::unique_ptr<::Run> state = std::make_unique<::Run>(*appMock);
+    state->enter();
+}
+
+TEST_F(StateTest, Should_InitCallLeave)
+{
+    EXPECT_CALL(*appMock, reportApplicationState(_)).WillOnce(Return());
+
+    std::unique_ptr<Init> state = std::make_unique<Init>(*appMock);
+    state->leave();
+}
+
+TEST_F(StateTest, Should_TerminateCallEnter)
+{
+    EXPECT_CALL(*appMock, reportApplicationState(_)).WillOnce(Return());
+
+    std::unique_ptr<Terminate> state = std::make_unique<Terminate>(*appMock);
+    state->enter();
+}

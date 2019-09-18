af7cc79e62ae49377791da7dd18e9de9d9cd412a
Uliana Stashkiv <54033925+ulianaStashkiv@users.noreply.github.com>
2019-09-18 11:42:34 +0300

    Add test for msm & msm state machine.

--- applications/MachineStateManager/test/msm_test.cpp
+++ applications/MachineStateManager/test/msm_test.cpp
@@ -1,10 +1,92 @@
-#include "gtest/gtest.h"
 #include "machine_state_manager.hpp"
+#include "msm_state_machine.hpp"
 
-// Dummy test case
-TEST(MachineStateManagerTest, test)
+#include <atomic>
+#include <iostream>
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using namespace testing;
+using namespace MachineStateManager;
+
+class IStateFactoryMock : public api::IStateFactory
 {
-  //MachineStateManager::MachineStateManager msm;
+public:
+    MOCK_METHOD(std::unique_ptr<api::IState>, createInit, (api::IAdaptiveApp &msm));
+    MOCK_METHOD(std::unique_ptr<api::IState>, createRun, (api::IAdaptiveApp &msm));
+    MOCK_METHOD(std::unique_ptr<api::IState>, createShutDown, (api::IAdaptiveApp &msm));
+};
 
-  ASSERT_EQ(1, 1);
+class StateClientMock : public api::ApplicationStateClientWrapper
+{
+public:
+    MOCK_METHOD(void, ReportApplicationState, (ApplicationStateManagement::ApplicationState state));
+};
+
+class IStateMock : public api::IState
+{
+public:
+    MOCK_METHOD(void, enter, ());
+    MOCK_METHOD(void, leave, (), (const));
+};
+
+class MsmTest : public ::testing::Test
+{
+protected:
+   std::unique_ptr<StateClientMock> stateClientMock{std::make_unique<StateClientMock>()};
+   std::unique_ptr<IStateFactoryMock> factoryMock{std::make_unique<IStateFactoryMock>()};
+   std::unique_ptr<NiceMock<IStateMock>> stateInitMock = std::make_unique<NiceMock<IStateMock>>();
+   std::unique_ptr<NiceMock<IStateMock>> stateRunMock = std::make_unique<NiceMock<IStateMock>>();
+   std::unique_ptr<NiceMock<IStateMock>> stateTermMock = std::make_unique<NiceMock<IStateMock>>();
+};
+
+TEST_F(MsmTest, Should_TransitToInitState)
+{
+    EXPECT_CALL(*factoryMock,
+                createInit((_)))
+            .WillOnce(Return(ByMove(std::move(stateInitMock))));
+
+     MachineStateManager::MachineStateManager msm{std::move(factoryMock), nullptr};
+     msm.init();
+
+}
+
+TEST_F(MsmTest, Should_TransitToRunState)
+{
+    EXPECT_CALL(*factoryMock,
+                createInit((_)))
+            .WillOnce(Return(ByMove(std::move(stateInitMock))));
+
+    EXPECT_CALL(*factoryMock,
+                createRun((_)))
+            .WillOnce(Return(ByMove(std::move(stateRunMock))));
+
+     MachineStateManager::MachineStateManager msm{std::move(factoryMock),
+                     nullptr};
+     msm.init();
+     msm.run();
+}
+
+TEST_F(MsmTest, Should_TransitToTerminateState)
+{
+    EXPECT_CALL(*factoryMock,
+                createInit((_)))
+            .WillOnce(Return(ByMove(std::move(stateInitMock))));
+    EXPECT_CALL(*factoryMock,
+                createShutDown((_)))
+            .WillOnce(Return(ByMove(std::move(stateTermMock))));
+
+     MachineStateManager::MachineStateManager msm{std::move(factoryMock),
+                     nullptr};
+     msm.init();
+     msm.terminate();
+}
+
+TEST_F(MsmTest, Should_ReportCurrentState)
+{
+    EXPECT_CALL(*stateClientMock, ReportApplicationState(_)).WillOnce(Return());
+
+    MachineStateManager::MachineStateManager msm{nullptr, std::move(stateClientMock)};
+    msm.reportApplicationState(api::ApplicationStateClient::ApplicationState::K_RUNNING);
 }

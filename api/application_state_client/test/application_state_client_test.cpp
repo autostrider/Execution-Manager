5c2bb8c648eb57b10e3308f6594a270d6b25bc30
KateTheDev <katusha.etoya@gmail.com>
2019-09-20 16:38:09 +0300

    Changes after review

--- api/application_state_client/test/application_state_client_test.cpp
+++ api/application_state_client/test/application_state_client_test.cpp
@@ -10,63 +10,61 @@ using namespace std;
 
 namespace 
 {
-    struct Data
-    {
-        ::ApplicationStateManagement::ApplicationState m_state;
-        pid_t m_appPid;
-    };
+struct Data
+{
+    ::ApplicationStateManagement::ApplicationState m_state;
+    pid_t m_appPid;
+};
 
-    class ApplicationStateClientServer : public ::ExecutionManagement::Server
-    {
-      using ApplicationState = ::ApplicationStateManagement::ApplicationState;
+class ApplicationStateClientServer : public ::ExecutionManagement::Server
+{
+    using ApplicationState = ::ApplicationStateManagement::ApplicationState;
 
-        public:
+public:
+    explicit
+    ApplicationStateClientServer (Data& sharedResource);
 
-        explicit
-        ApplicationStateClientServer (Data& sharedResource);
-
-        private:
-        ::kj::Promise<void>
-        reportApplicationState(ReportApplicationStateContext context) override;
-
-        private:
-
-        Data& m_sharedResource;
-    };
-
-    ApplicationStateClientServer::ApplicationStateClientServer (Data& sharedResource) : m_sharedResource{sharedResource}
-    {
-        cout << "Application State Client server started..." << endl;
-    }
-
+private:
     ::kj::Promise<void>
-    ApplicationStateClientServer::reportApplicationState(ReportApplicationStateContext context)
-    {
-        m_sharedResource.m_state = context.getParams().getState();
-        m_sharedResource.m_appPid = context.getParams().getPid();
+    reportApplicationState(ReportApplicationStateContext context) override;
+
+private:
+
+    Data& m_sharedResource;
+};
+
+ApplicationStateClientServer::ApplicationStateClientServer (Data& sharedResource) : m_sharedResource{sharedResource}
+{
+    cout << "Application State Client server started..." << endl;
+}
+
+::kj::Promise<void>
+ApplicationStateClientServer::reportApplicationState(ReportApplicationStateContext context)
+{
+    m_sharedResource.m_state = context.getParams().getState();
+    m_sharedResource.m_appPid = context.getParams().getPid();
          
-        return kj::READY_NOW;
-    }
+    return kj::READY_NOW;
+}
 
-	class ApplicationStateClientTest : public ::testing::Test 
+class ApplicationStateClientTest : public ::testing::Test 
+{
+protected:
+    virtual ~ApplicationStateClientTest() noexcept(true) {}
+
+    ApplicationStateClientTest() {}
+
+	virtual void SetUp()
 	{
-	    protected:
-	
-        virtual ~ApplicationStateClientTest() noexcept(true) {}
+		::unlink(socketName);
+	}
 
-        ApplicationStateClientTest() {}
+	virtual void TearDown() {}
 
-		virtual void SetUp()
-		{
-			::unlink(socketName);
-		}
-
-		virtual void TearDown() {}
-
-        Data sharedResource;  
-        const char* socketName = "/tmp/execution_management"; 
-        capnp::EzRpcServer server{kj::heap<ApplicationStateClientServer>(sharedResource), string{"unix:"} + socketName}; 
-	};
+    Data sharedResource;  
+    const char* socketName = "/tmp/execution_management"; 
+    capnp::EzRpcServer server{kj::heap<ApplicationStateClientServer>(sharedResource), string{"unix:"} + socketName}; 
+};
 
 TEST_F(ApplicationStateClientTest, ShouldSucceedToReportApplicationState)
 {
@@ -76,5 +74,3 @@ TEST_F(ApplicationStateClientTest, ShouldSucceedToReportApplicationState)
 	ASSERT_EQ(::ApplicationStateManagement::ApplicationState::K_RUNNING, sharedResource.m_state);
 }
 } //namespace
-
-

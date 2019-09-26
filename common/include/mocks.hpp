4cd876394b8be9f289711b865fb7a08270a60500
Uliana Stashkiv <54033925+ulianaStashkiv@users.noreply.github.com>
2019-09-26 16:18:47 +0300

    Update after review

--- common/include/mocks.hpp
+++ common/include/mocks.hpp
@@ -11,17 +11,17 @@ using StateError = api::MachineStateClient::StateError;
 
 class IMachineStateManagerMock : public MachineStateManager
 {
-    public:
-        IMachineStateManagerMock(std::unique_ptr<api::IStateFactory> factory,
-                                 std::unique_ptr<api::IApplicationStateClientWrapper> appClient,
-                                 std::unique_ptr<api::IMachineStateClientWrapper> machineClient);
+public:
+    IMachineStateManagerMock(std::unique_ptr<api::IStateFactory> factory,
+                             std::unique_ptr<api::IApplicationStateClientWrapper> appClient,
+                             std::unique_ptr<api::IMachineStateClientWrapper> machineClient);
 
-        MOCK_METHOD(void, init, ());
-        MOCK_METHOD(void, run, ());
-        MOCK_METHOD(void, terminate, ());
+    MOCK_METHOD(void, init, ());
+    MOCK_METHOD(void, run, ());
+    MOCK_METHOD(void, terminate, ());
 
-        MOCK_METHOD(void, reportApplicationState, (api::ApplicationStateClient::ApplicationState state));
-        MOCK_METHOD(void, transitToNextState, (IMachineStateManagerMock::FactoryFunc nextState));
+    MOCK_METHOD(void, reportApplicationState, (api::ApplicationStateClient::ApplicationState state));
+    MOCK_METHOD(void, transitToNextState, (IMachineStateManagerMock::FactoryFunc nextState));
 };
 
 class IStateFactoryMock : public api::IStateFactory

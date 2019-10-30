b952858098b82c7f977f03f48b9af929050e3cae
Uliana Stashkiv <54033925+ulianaStashkiv@users.noreply.github.com>
2019-10-30 15:02:11 +0200

    Updated according to the comments.

--- applications/AdaptiveApplication1/src/adaptive_app.cpp
+++ applications/AdaptiveApplication1/src/adaptive_app.cpp
@@ -53,8 +53,8 @@ void AdaptiveApp::transitToNextState(api::IAdaptiveApp::FactoryFunc nextState)
 }
 void AdaptiveApp::reportApplicationState(api::ApplicationStateClient::ApplicationState state)
 {
-  LOG << "gtrjkj" << static_cast<int>(state);
-   m_appClient->ReportApplicationState(state);
+    LOG << "Report application state: " << static_cast<int>(state);
+    m_appClient->ReportApplicationState(state);
 }
 
 api::ComponentClientReturnType AdaptiveApp::getComponentState(api::ComponentState &state) noexcept

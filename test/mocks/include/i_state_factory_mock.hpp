11db237de83abda5aba2fc49967e194838a20696
ulianaStashkiv <uliana.stashkiv@gmail.com>
2020-01-20 15:41:37 +0200

    Change project structure (file location).

---
+++ test/mocks/include/i_state_factory_mock.hpp
@@ -0,0 +1,23 @@
+#ifndef I_STATE_FACTORY_MOCK_HPP
+#define I_STATE_FACTORY_MOCK_HPP
+
+#include <i_state_factory.hpp>
+
+#include "gmock/gmock.h"
+
+namespace api
+{
+
+class StateFactoryMock : public IStateFactory
+{
+public:
+    StateFactoryMock() = default;
+    MOCK_CONST_METHOD1(createInit, std::unique_ptr<IState>(IAdaptiveApp& msm));
+    MOCK_CONST_METHOD1(createRun, std::unique_ptr<IState>(IAdaptiveApp& msm));
+    MOCK_CONST_METHOD1(createShutDown, std::unique_ptr<IState>(IAdaptiveApp& msm));
+    MOCK_CONST_METHOD1(createSuspend, std::unique_ptr<IState>(IAdaptiveApp& msm));
+};
+
+} // namespace api
+
+#endif // I_STATE_FACTORY_MOCK_HPP

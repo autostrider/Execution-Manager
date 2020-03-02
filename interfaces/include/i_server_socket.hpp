db7c4480d800a5b2f6b75e67400984ab3e78e3c1
ulianaStashkiv <uliana.stashkiv@gmail.com>
2020-03-02 15:41:35 +0200

    Create folder "interfaces" for common interfaces.

---
+++ interfaces/include/i_server_socket.hpp
@@ -0,0 +1,18 @@
+#ifndef EXECUTION_MANAGER_I_SERVER_SOCKET_HPP
+#define EXECUTION_MANAGER_I_SERVER_SOCKET_HPP
+
+#include "i_socket.hpp"
+
+class IServerSocket : public ISocket
+{
+public:
+    virtual ~IServerSocket() = default;
+
+    virtual int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) const = 0;
+    virtual int listen(int sockfd, int backlog) const = 0;
+    virtual int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen) const = 0;
+    virtual int fcntl(int fd, int cmd, int arg) const = 0;
+    virtual int remove(const char *path) const = 0;
+};
+
+#endif //EXECUTION_MANAGER_I_SERVER_SOCKET_HPP
